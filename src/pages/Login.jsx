6a6db92d42db31af274ac9f47d88d40a0c7aef0f
Betul <betulkoru06@gmail.com>
2024-03-25 01:59:41 +0300

    login page and tailwind css

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,8 +2,39 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div>
+      <div>
+        <div>
+          <span></span>
+          <h1>Hanako Store</h1>
+        </div>
+        <div>
+          <h3>SIGN IN</h3>
+          <p>Enter your credentials to access your account</p>
+        </div>
+        <form>
+          <div className="">
+            <label htmlFor="email">Email</label>
+            <input type="email" placeholder="Enter your email" required />
+          </div>
+          <div>
+            <label htmlFor="pass">Password</label>
+            <input
+              type="password"
+              name="pass"
+              placeholder="Enter your password"
+              required
+            />
+          </div>
+          <button>Sign In</button>
+          <p>
+            <span> Forgot your password? </span>
+            <span>Reset Password</span>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default Login
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,27 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: "#6a0d83",
+        navbarColor: "#ce4993",
+        labelColor: "#fb9062",
+        white: "#fff"
+      },
+      fontFamily: {
+        'syne' : ['"Syne"', 'sans-serif'],
+      },
+      fontSize: {
+        label: [ '14px', {
+          lineHeight: 'normal',
+          fontWeight: '500',
+        }],
+      },
+      boxShadow: {
+        'login': '2px 5px 10px 0px rgba(0, 0, 0, 0.1)',
+      }
+    },
   },
   plugins: [],
-}
+};
 
