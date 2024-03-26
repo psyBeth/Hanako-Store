1c30c418b4918d01cdb621deb2a12a57b23f7e34
Betul <betulkoru06@gmail.com>
2024-03-27 02:32:17 +0300

    products intro

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import SearchInput from "../components/SearchInput";
+import ProductCard from "../components/ProductCard";
 
 const Products = () => {
+  
+
   return (
-    <div>Products</div>
+    <div className="mx-auto max-w-2x1 px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7x1 lg:px-8">
+      <SearchInput />
+      <h2 className="text-2xl font-bold mt-8 tracking-tight text-gray-900">
+        All Products
+      </h2>
+      <div>
+        <ProductCard />
+      </div>
+    </div>
   )
 }
 
