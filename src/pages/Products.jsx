bb2d69530af5a5f2d174b7fe6ac39b5532dbe148
Betul <betulkoru06@gmail.com>
2024-03-27 03:12:50 +0300

    card-div applied

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -27,7 +27,7 @@ const Products = () => {
   }, [search]);  //! getData() whenever the search state changes
 
   return (
-    <div className="mx-auto max-w-2x1 px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7x1 lg:px-8">
+    <div className="container">
       <SearchInput search={search} setSearch={setSearch} />
       <h2 className="text-2xl font-bold mt-8 tracking-tight text-gray-900">
         All Products
