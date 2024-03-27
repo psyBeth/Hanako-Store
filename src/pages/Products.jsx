2be922f012ec0bb981b1adb2b75b7755a4ed9776
Betul <betulkoru06@gmail.com>
2024-03-27 03:01:07 +0300

    products loading

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -2,6 +2,7 @@ import SearchInput from "../components/SearchInput";
 import ProductCard from "../components/ProductCard";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import Loading from "../components/Loading";
 
 const Products = () => {
   const [products, setProducts] = useState([]);
@@ -31,9 +32,20 @@ const Products = () => {
       <h2 className="text-2xl font-bold mt-8 tracking-tight text-gray-900">
         All Products
       </h2>
-      <div>
-        <ProductCard />
-      </div>
+      {loading ? (
+        <Loading />
+      ) : products.length ? (
+        // <div className="mt-6 grid grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10 xl:gap-x-8">
+        <div className="card-div">
+          {products.map((item) => (
+            <ProductCard key={item.id} item={item} />
+          ))}
+        </div>
+      ) : (
+        <h2 className="text-center text-3xl text-red-600 mt-32">
+          No Products
+        </h2>
+      )}
     </div>
   )
 }
