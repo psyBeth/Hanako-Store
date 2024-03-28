531f83e2b1ba153b126e0accf5f0c664ef85045d
Betul <betulkoru06@gmail.com>
2024-03-28 03:00:11 +0300

    product provider contect struct

--- src/context/ProductProvider.jsx
+++ src/context/ProductProvider.jsx
@@ -1,11 +1,43 @@
 import { createContext } from "react";
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
+
 
 const ProductContext = createContext();
 
 const ProductProvider = ({ children }) => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [search, setSearch] = useState("");
+
+  const getData = async () => {
+    setLoading(true);
+    try {
+      const { data } = await axios(`https://dummyjson.com/products/search?q=${search}`);
+      // console.log(data);
+      setProducts(data.products);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getData()
+  }, [search]);
   return (
-    <div>ProductProvider</div>
+    <ProductContext.Provider value={{ products, loading, search, setSearch }}>
+      product provider
+    </ProductContext.Provider>
   )
 }
 
-export default ProductProvider
+export default ProductProvider;
+
+export const useProducts = () => {
+  return useContext(ProductContext)
+};
+//! custom hooks are needed bcs react hooks cannot be used in javascript functions
+//? custom hooks must start with use
+//* custom hooks does not return jsx
